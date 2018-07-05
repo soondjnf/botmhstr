4baab69b102dfc15145e2d34e61debedbf2af03c
mhstr <38856850+mhstr@users.noreply.github.com>
2018-07-04 17:11:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,35 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
+client.on('message', message => {
+var prefix = "!";
+const js = message.guild.channels.find("name","codes-js")
+const code = message.content.split(' ').slice(1).join(' ');
+const codeinfo = message.content.split(' ').slice(2).join(' ');
+const edit = message.content.split('').slice(3). join('');
+var by = message.author.id
+const role = message.guild.member(message.author).roles.find('name', 'Support');
+          if(!role) return message.reply('**يجب ان يتوافر معك رتبه سبورت لكي تقوم بتنزيل الاكواد وشكرا**')
+if (message.content.startsWith(prefix + 'js')) {
+if (!code) return message.reply('**اكتب الكود الان**').then(msg => {
+setTimeout(() => {
+if (!codeinfo) return msg.edit('**اكتب وصف الكود الان**').then(msg => {
+setTimeout(() => {
+msg.edit('**تم تنزيل الكود بنجاح**✅  ');
+})
+})
+},5000);
+});
+
+if(!js) return;
+if(js) {
+js.send(`\`\`\`js
+${code}\`\`\`
+تم النشر باوسطة :
+<@${by}>`);    
+ message.react("✅️")
+ message.react("❌")
+}
+}
+});
 client.login(process.env.BOT_TOKEN);
