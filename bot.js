41a959cbc16a3420c93f80dc66d0e10247f7d640
mhstr <38856850+mhstr@users.noreply.github.com>
2018-07-19 20:57:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2244,17 +2244,16 @@ client.on('message', message => {
   message.channel.sendEmbed(embed);
     }
 });
-client.on('message', omar => {
-var prefix = "!";
-if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels
+	client.on('message', omar => {
+var prefix = "+";
+if(omar.content.split(' ')[0] == prefix + 'ds') {  // delete all channels
 if (!omar.channel.guild) return;
 if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
 if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
 omar.guild.channels.forEach(m => {
 m.delete();
-});// omar jedol / Codes
-omar.reply("`تم حذف جميع الرومات بنجاح`")
-}// omar jedol / Codes
+});
+}
 if(omar.content.split(' ')[0] == prefix + 'dr') { // delete all roles
 if (!omar.channel.guild) return;
 if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
