40b17878577174bdf20875bb51e3de4da9544392
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-08 11:54:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const giphy = require('giphy-api')();
 const googl = require('goo.gl');  
 const translate = require('google-translate-api');   
 const fs = require("fs"); 
+const canvas = require("canvas");
 const getYoutubeID = require('get-youtube-id'); 
 const moment = require("moment");  
 const { Client, Util } = require('discord.js');  
@@ -23,6 +24,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
  const dateFormat = require('dateformat'); 
  const pretty = require('pretty-ms') 
+
 ,ti={}  
 ,spee={};
 
