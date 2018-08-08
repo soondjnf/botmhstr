e69815e957c516321b8a9cf3be688c60311aa99f
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-08 15:58:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,4 +240,44 @@ if (message.content.startsWith(adminprefix + 'setT')) {
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 });
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix+'help')) {
+   const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+        .setDescription(`
+
+        ***__Music orders__***
+**
+『${prefix}play / لتشغيل أغنية برآبط أو بأسم』
+『${prefix}skip / لتجآوز الأغنية الحآلية』
+『${prefix}pause / إيقآف الأغنية مؤقتا』
+『${prefix}resume / لموآصلة الإغنية بعد إيقآفهآ مؤقتا』
+『${prefix}vol / لتغيير درجة الصوت 100 - 0』
+『${prefix}stop / لإخرآج البوت من الروم』
+『${prefix}np / لمعرفة الأغنية المشغلة حآليا』
+『${prefix}queue / لمعرفة قآئمة التشغيل』**`)
+    message.author.send(embed)
+}
+});
+
+client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , "شيك على الخاص يا حلو")
+  message.channel.sendEmbed(embed);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
