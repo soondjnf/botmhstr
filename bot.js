e61e4b394668dc392887083ea3a19c43d12fa8df
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-08 11:51:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ const sql = require("sqlite");
 ,spee={};
 
 const prefix = "6"
+
 client.on('message', async msg => { 
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(prefix)) return undefined;
